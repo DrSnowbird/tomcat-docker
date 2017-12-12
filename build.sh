08138ec3ed100cfc9f909e82cb1b83513c08e012
DrSnowbird <DrSnowbird@openkbs.org>
2017-12-12 00:39:39 -0500

    update build.sh run.sh Tue Dec 12 00:39:38 EST 2017

--- build.sh
+++ build.sh
@@ -1,3 +1,41 @@
-#!/bin/bash -x
+#!/bin/bash 
 
-docker build -t openkbs/jre-tomcat .
+# Reference: 
+# - https://docs.docker.com/engine/userguide/containers/dockerimages/
+# - https://github.com/dockerfile/java/blob/master/oracle-java8/Dockerfile
+
+if [ $# -lt 1 ]; then
+    echo "Usage: "
+    echo "  ${0} <image_tag>"
+fi
+
+###################################################
+#### **** Container package information ****
+###################################################
+DOCKER_IMAGE_REPO=`echo $(basename $PWD)|tr '[:upper:]' '[:lower:]'|tr "/: " "_" `
+imageTag=${1:-"openkbs/${DOCKER_IMAGE_REPO}"}
+
+docker build --rm -t ${imageTag} \
+	-f Dockerfile .
+
+echo "----> Shell into the Container in interactive mode: "
+echo "  docker exec -it --name <some-name> /bin/bash"
+echo "e.g."
+echo "  docker run --name "my-$(basename $imageTag)" /bin/bash "
+
+echo "----> Run: "
+echo "  docker run --name <some-name> -it ${imageTag} /bin/bash"
+echo "e.g."
+echo "  docker run --name "my-$(basename $imageTag)" ${imageTag} "
+
+echo "----> Run in interactive mode: "
+echo "  docker run -it --name <some-name> ${imageTag} /bin/bash"
+echo "e.g."
+echo "  docker run -it --name "my-$(basename $imageTag)" -it ${imageTag} "
+
+echo "----> Build Docker Images again: "
+echo "To build again: (there is a dot at the end of the command!)"
+echo "  docker build -t ${imageTag} . "
+echo
+docker images |grep "$imageTag"
+
--- run.sh
+++ run.sh
@@ -1,8 +1,91 @@
-#!/bin/bash -x
+#!/bin/bash
 
-if [ $# -gt 0 ]; then
-    docker run -d -p 8080:8080 -e TOMCAT_PASS="$1" openkbs/jre-tomcat
-else
-    docker run -d -p 8080:8080 openkbs/jre-tomcat
+if [ $# -lt 1 ]; then
+    echo "Usage: "
+    echo "  ${0} <image_tag>"
 fi
+
+###################################################
+#### ---- Change Tomcat admin password ----
+###################################################
+TOMCAT_PASS=${TOMCAT_PASS:-password12345}
+
+###################################################
+#### **** Container package information ****
+###################################################
+MY_IP=`ip route get 1|awk '{print$NF;exit;}'`
+DOCKER_IMAGE_REPO=`echo $(basename $PWD)|tr '[:upper:]' '[:lower:]'|tr "/: " "_" `
+imageTag=${1:-"openkbs/${DOCKER_IMAGE_REPO}"}
+#PACKAGE=`echo ${imageTag##*/}|tr "/\-: " "_"`
+PACKAGE="${imageTag##*/}"
+baseDataFolder="$HOME/data-docker"
+
+###################################################
+#### ---- Volumes to be mapped (change this!) -----
+#################################################### (examples - some local vars)
+# (exampples)
+# IDEA_PRODUCT_NAME="IdeaIC2017"
+# IDEA_PRODUCT_VERSION="3"
+# IDEA_INSTALL_DIR="${IDEA_PRODUCT_NAME}.${IDEA_PRODUCT_VERSION}"
+# IDEA_CONFIG_DIR=".${IDEA_PRODUCT_NAME}.${IDEA_PRODUCT_VERSION}"
+# IDEA_PROJECT_DIR="IdeaProjects"
+# VOLUMES="${IDEA_CONFIG_DIR} ${IDEA_PROJECT_DIR}"
+
+# ---------------------------
+# MANDATORY Variable: VOLUMES
+# ---------------------------
+VOLUMES="data workspace"
+
+#########################################################################################################
+######################## DON'T CHANGE LINES STARTING BELOW (unless you need to) #########################
+#########################################################################################################
+LOCAL_VOLUME_DIR="${baseDataFolder}/${PACKAGE}"
+DOCKER_VOLUME_DIR="/home/developer"
+
+###################################################
+#### ---- Function: Generate volume mappings  ----
+####      (Don't change!)
+###################################################
+VOLUME_MAP=""
+#### Input: VOLUMES - list of volumes to be mapped
+function generateVolumeMapping() {
+    for vol in $VOLUMES; do
+        echo "$vol"
+        if [[ $vol == "/"* ]]; then
+            # -- non-default /home/developer path; then use the full absolute path --
+            VOLUME_MAP="${VOLUME_MAP} -v ${LOCAL_VOLUME_DIR}/$vol:$vol"
+        else
+            # -- default sub-directory (without prefix absolute path) --
+            VOLUME_MAP="${VOLUME_MAP} -v ${LOCAL_VOLUME_DIR}/$vol:${DOCKER_VOLUME_DIR}/$vol"
+        fi
+        mkdir -p ${LOCAL_VOLUME_DIR}/$vol
+        ls -al ${LOCAL_VOLUME_DIR}/$vol
+    done
+}
+#### ---- Generate Volumes Mapping ----
+generateVolumeMapping
+echo ${VOLUME_MAP}
+
+###################################################
+#### ---- Mostly, you don't need change below ----
+###################################################
+
+#instanceName=my-${1:-${imageTag%/*}}_$RANDOM
+#instanceName=my-${1:-${imageTag##*/}}
+## -- transform '-' and space to '_' 
+#instanceName=`echo $(basename ${imageTag})|tr '[:upper:]' '[:lower:]'|tr "/\-: " "_"`
+instanceName=`echo $(basename ${imageTag})|tr '[:upper:]' '[:lower:]'|tr "/: " "_"`
+
+echo "---------------------------------------------"
+echo "---- Starting a Container for ${imageTag}"
+echo "---------------------------------------------"
+DISPLAY=${MY_IP}:0 \
+docker run -ti --rm \
+    --name=${instanceName} \
+    -e DISPLAY=$DISPLAY \
+    -e TOMCAT_PASS="${TOMCAT_PASS}" 
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    ${VOLUME_MAP} \
+    ${imageTag}
+
 
